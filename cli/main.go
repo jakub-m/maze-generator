1474f4ce6c052d0136395ea40d510403b4393755
Jakub Mikians <jakub.mikians@gmail.com>
2021-06-06 18:08:16 +0200

    flags

--- cli/main.go
+++ cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,20 +12,47 @@ import (
 	"time"
 )
 
-const scale = 50
+var size int
+var seed int64
+var outfname string
+var scale int
+
+func init() {
+	flagScale := flag.Int("scale", 50, "scale")
+	flagSeed := flag.Int64("seed", time.Now().Unix(), "random seed")
+	flagSize := flag.Int("size", 6, "size")
+	flagOutfile := flag.String("out", "-", "output filename or \"-\"")
+	flagVerbose := flag.Bool("verbose", false, "verbose mode")
+	flag.Parse()
+
+	scale = *flagScale
+	seed = *flagSeed
+	size = *flagSize
+	outfname = *flagOutfile
+	if *flagVerbose {
+		log.SetOutput(os.Stderr)
+	} else {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+}
 
 func main() {
-	rand.Seed(time.Now().Unix())
-	//rand.Seed(0)
-	log.SetOutput(os.Stderr)
-	log.Print("logger works!")
-	n := 20
-	c := cell.NewDividedCell(n, n)
+	log.Println("scale", scale)
+	log.Println("seed", seed)
+	log.Println("size", size)
+	log.Println("out", outfname)
+	rand.Seed(seed)
+	c := cell.NewDividedCell(size, size)
 	f, err := svg.Format(c, scale)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	ioutil.WriteFile("tmp.svg", f, 0644)
-	log.Print("done")
+	if outfname == "-" {
+		os.Stdout.Write(f)
+	} else {
+		ioutil.WriteFile(outfname, f, 0644)
+	}
+	log.Println("done")
 }
